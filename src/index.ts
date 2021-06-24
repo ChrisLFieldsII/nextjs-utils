0e29cbb37bf4d08846d1aa6c189d579e5d322196
Christopher Fields <chrisjr06@gmail.com>
2021-06-23 19:36:44 -0500

    feat: export some modules from utils pkg for ease

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 export * from './types';
 export * from './api-handler';
+
+// export some modules from utils pkg. be careful to not include browser dependent modules.
+export {
+  ApiError,
+  ErrorWithCode,
+  getError,
+  getListResponse,
+  noop,
+  renderNull,
+} from '@chrisfieldsii/utils';
